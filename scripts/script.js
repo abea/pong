67396f5770ee2b9f6a0475d0b13b9d6f102a52ec
Alex Bea <alex.bea@gmail.com>
2015-07-16 14:59:55 -0400

    paddle now controlled by up/down arrows;

--- scripts/script.js
+++ scripts/script.js
@@ -1,95 +1,130 @@
 // width="640px" height="480px" 
- 
- (function(){
-  "use strict";
-// USE STRICT START
-
-(function(){ 
-//var paddlerFactory = function (paddle) {
-//  return {
-//    paddle: paddle;
-//  };
-//};
-//var player = paddlerFactory(
-//  new Paddle(460, 240, 8, 32);
-//);
-//var computer = paddlerFactory(
-//  new Paddle()
-//)
-//   
-//var player = {
-//  setPaddle: function (paddle) {
-//    this.paddle = paddle;
-//  }
-//}
-})(); // To hide scraps; Remove on completion;
-$( document ).ready(function() {
-
-var field = document.getElementById('field');
-var context = field.getContext("2d");
-// DRAW MID-LINE
-context.beginPath();
-context.moveTo(320, 0);
-context.lineTo(320, 480);
-context.strokeStyle = 'white';
-context.lineWidth = 8;
-context.stroke();
-// END MID-LINE  
-
-// PADDLE / PLAYERS
-function Paddle(x, y, width, height) {
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
-}
-function Player(paddle) {
-  this.paddle = paddle;
-  this.render = function () {
-    context.fillStyle = 'black';
-    context.beginPath();
-    context.fillRect(this.paddle.x, this.paddle.y, this.paddle.width, this.paddle.height);
-  };
-}
-function Computer(paddle) {
-  this.paddle = paddle;
-  this.render = function () {
-    context.fillStyle = 'black';
-    context.beginPath();
-    context.fillRect(this.paddle.x, this.paddle.y, this.paddle.width, this.paddle.height);
-  };
-}
-var player = new Player (
-  new Paddle(620, 224, 8, 32) // Right side
-);
-var computer = new Computer (
-  new Paddle(20, 224, 8, 32) // Left side
-);
-
   
-// BALL
-function Ball(x, y) {
-  this.x = x;
-  this.y = y;
-  this.render = function (){
-    context.fillStyle = 'black';
-    context.beginPath();
-    context.arc(this.x, this.y, 6, 0, 2 * Math.PI);
-    context.closePath();
-    context.fill();
-  };
-}
-var ball = new Ball(160, 240);
-function render () {
-  player.render();
-  computer.render();
-  ball.render();
-}
-render();
+  $(document).ready(function() {
 
-// RESET AFTER POINT
-//field.width = field.width;
+    var field = document.getElementById('field'),
+        context = field.getContext("2d"),
+        midWidth = (field.width / 2),
+        midHeight = (field.height / 2);
+    var kickOffPoint = {
+      x : (midWidth / 2),
+      y : 240
+    };
+    
+    function drawField() {
+      context.beginPath();
+      context.moveTo(midWidth, 0);
+      context.lineTo(midWidth, field.height);
+      context.strokeStyle = 'white';
+      context.lineWidth = 8;
+      context.stroke();
+    }
 
-}); // END DOCUMENT.READY
-// USE STRICT CLOSE
-}());
+
+    // PADDLE / PLAYERS
+
+    function Paddle(x, y, width, height) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      var increment = 15;
+      this.move = function(event) {
+        switch (event.key) {
+          case 'ArrowUp':
+//            console.log('up' + this.y);
+            if (this.y > increment ) {
+              this.y = this.y - increment;
+            } else if (this.y > 0 ) {
+              this.y = this.y - this.y;
+            }
+            break;
+          case 'ArrowDown':
+//            console.log('down ' + this.y );
+            if (this.y > (field.height-(this.height + increment))) {
+              this.y = this.y + (field.height - (this.y + this.height));
+            } else if (this.y < (field.height-this.height)) {
+              this.y = this.y + increment;
+            }
+            break;
+        }
+        return;
+      };
+      
+      this.getPos = function() {return [this.x, this.y]};
+    }
+
+    function Player(playerType, paddle) {
+      this.playerType = playerType;
+      this.paddle = paddle;
+      this.render = function() {
+        context.fillStyle = 'black';
+        context.beginPath();
+        context.fillRect(this.paddle.x, this.paddle.y, this.paddle.width, this.paddle.height);
+      };
+      
+      var movePaddle = function(e) {
+        this.paddle.move(e);
+        console.log(this.paddle.getPos());
+      };
+      this.movePaddle = movePaddle;
+    }
+    var rightSide = field.width - 20,
+        leftSide = 20,
+        paddleWidth = 8,
+        paddleHeight = 40;
+    var player = new Player(
+      'human', 
+      new Paddle(rightSide, (midHeight - (paddleHeight/2)), paddleWidth, paddleHeight)
+    );
+    var computer = new Player(
+      'computer', 
+      new Paddle(leftSide, (midHeight - (paddleHeight/2)), paddleWidth, paddleHeight)
+    );
+
+    // BALL
+    function Ball(placeBall) {
+      this.x = placeBall.x;
+      this.y = placeBall.y;
+      this.render = function() {
+        context.fillStyle = 'black';
+        context.beginPath();
+        context.arc(this.x, this.y, 6, 0, 2 * Math.PI);
+        context.closePath();
+        context.fill();
+      };
+    }
+
+    var ball = new Ball(kickOffPoint);
+
+    function render() {
+        player.render();
+        computer.render();
+        ball.render();
+      }
+      // PADDLE CONTROL
+    var animate = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame || 
+        window.mozRequestAnimationFrame || 
+        window.oRequestAnimationFrame || 
+        window.msRequestAnimationFrame || function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+
+    function step() {
+      field.width = field.width;
+      drawField();
+      render();
+      animate(step);
+    }
+  function gameInit(){
+
+
+  }
+  //$(document).ready(function() {
+//    gameInit();
+    
+    step();
+//    window.addEventListener("keydown", player.paddle.move);
+    window.addEventListener("keypress", function(e) {player.movePaddle(e);});
+  }); // END DOC.READY
